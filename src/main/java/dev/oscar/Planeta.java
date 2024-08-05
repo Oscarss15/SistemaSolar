1ed0af985bf7130e45584d5138c5433d552031d5
Oscar <oscar15-91@hotmail.com>
2024-08-05 09:23:00 +0200

    creado atributos

---
+++ src/main/java/dev/oscar/Planeta.java
@@ -0,0 +1,19 @@
+package dev.oscar;
+
+public class Planeta {
+    
+     String nombre=null;
+     int cantSatelites = 0;
+     double masa= 0;
+     double volumen =0;
+     int diametro =0;
+     int distanciamediaSol= 0;
+     enum tipoPlaneta {GASEOSO, TERRESTRE, ENANO}
+     tipoPlaneta tipo;
+     boolean observable =false;
+
+
+
+
+
+}
