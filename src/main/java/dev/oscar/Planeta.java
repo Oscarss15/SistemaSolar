bf1d33dd98a88ce1f90da190a4043c259a8cc12a
Oscar <oscar15-91@hotmail.com>
2024-08-06 11:10:44 +0200

    añadido ejercicio con java,test y diagrama

--- src/main/java/dev/oscar/Planeta.java
+++ src/main/java/dev/oscar/Planeta.java
@@ -11,10 +11,15 @@ public class Planeta {
      enum tipoPlaneta {GASEOSO, TERRESTRE, ENANO}
      tipoPlaneta tipo;
      boolean observable =false;
+     int periodoOrbital=0;
+     int rotacion=0;
 
 
-    public Planeta(String nombre, int cantSatelites, double masa, double volumen, int diametro, int distanciamediaSol,
-            tipoPlaneta tipo, boolean observable) {
+   
+
+
+public Planeta(String nombre, int cantSatelites, double masa, double volumen, int diametro, int distanciamediaSol,
+            tipoPlaneta tipo, boolean observable, int periodoOrbital, int rotacion) {
         this.nombre = nombre;
         this.cantSatelites = cantSatelites;
         this.masa = masa;
@@ -23,6 +28,8 @@ public class Planeta {
         this.distanciamediaSol = distanciamediaSol;
         this.tipo = tipo;
         this.observable = observable;
+        this.periodoOrbital = periodoOrbital;
+        this.rotacion = rotacion;
     }
 
 
@@ -35,7 +42,40 @@ System.out.println("Diámetro del planeta = " + diametro);
 System.out.println("Distancia al sol = " + distanciamediaSol);
 System.out.println("Tipo de planeta = " + tipo);
 System.out.println("Es observable = " + observable);
+System.out.println("Periodo orbital en años = " + observable);
+System.out.println("Rotacion en dias = " + observable);
 }
 
+
+double calcularDensidad(){
+    return masa/volumen;
+}
+
+boolean esExterior(){
+    float limite = (float)(149597870 * 3.4);
+
+    if(distanciamediaSol > limite){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+public static void main(String args[]) {
+
+    
+        Planeta planeta1 = new Planeta("planeta1",1,5.9736E24,1.00000
+        ,12000,150000000,tipoPlaneta.TERRESTRE,true,365,24);
+        planeta1.imprimir();
+        Planeta planeta2 = new Planeta("planeta2",1,6.0036E24,2.00000
+        ,20000,200000000,tipoPlaneta.GASEOSO,false,400,40);
+        planeta2.imprimir();
+
+        System.out.println("La densidad del planeta1 es "+planeta1.calcularDensidad());
+        System.out.println("La densidad del planeta2 es "+planeta2.calcularDensidad());
+        System.out.println("¿el planeta1 es considerado exterior? "+planeta1.esExterior());
+        System.out.println("¿el planeta2 es considerado exterior? "+planeta2.esExterior());
+
+}
 
 }
